4a08927db52b0e3ae7ccccb9b1bc458b918d9f34
Tinotenda Rusike <Tino.Rusike@iam.local>
2018-09-18 15:18:59 -0500

    Added Placeholder Movement

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
+    public Rigidbody2D CharacterRigid;
 
 	// Use this for initialization
 	void Start () {
-		
+        Debug.Log("This is start!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKey(KeyCode.D)) {
+            //Moves Character to the right
+            CharacterRigid.velocity = new Vector2(5, CharacterRigid.velocity.y);
+        }
+        //Syntax for printing to the console!
+        //Debug.Log("Test");
 	}
 }
