000a11f23dc488786b339c632ee9ff34e8d51378
Tinotenda Rusike <Tino.Rusike@iam.local>
2018-09-25 15:18:19 -0500

    Edited Player Movement

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -3,20 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
-    public Rigidbody2D CharacterRigid;
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("This is start!");
+    [SerializeField]
+    private Rigidbody2D CharacterRigid;
+
+    [SerializeField]
+ private float Speed = 2;
+
+    private float moveInput;
+
+    // Use this for initialization
+    void Start () {
+
+        //Debug.Log("This is start!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.D)) {
-            //Moves Character to the right
-            CharacterRigid.velocity = new Vector2(5, CharacterRigid.velocity.y);
-        }
-        //Syntax for printing to the console!
-        //Debug.Log("Test");
+        GetMovementInput();
 	}
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GetMovementInput() {
+        moveInput = Input.GetAxis("Horizontal");
+
+    }
+    private void Move()
+    {
+
+    }
+
+    private void Jump()
+    {
+        //TODO: Make the Character Jump
+    }
 }
