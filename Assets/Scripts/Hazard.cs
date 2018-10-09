2daa15366c4f0c551ebdbf9bb1de25b8f0f1d3ed
Tinotenda Rusike <Tino.Rusike@iam.local>
2018-10-09 14:49:43 -0500

    Added Checkpoints, Hazards and Bug Fixes

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazard : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered the Hazard");
+            // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
+            player.Respawn();
+        }
+        else {
+            Debug.Log("Something entered the Hazard");
+        }
+        
+    }
+}
--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerCharacter : MonoBehaviour {
+public class PlayerCharacter : MonoBehaviour
+{
 
     [SerializeField]
     private Rigidbody2D CharacterRigid;
@@ -22,18 +24,27 @@ public class PlayerCharacter : MonoBehaviour {
     [SerializeField]
     private Collider2D groundDetectTrigger;
 
+    [SerializeField]
+    private PhysicsMaterial2D playerMoving, playerStopping;
+
+    [SerializeField]
+    private Collider2D playerGroundCollider;
+
     private float horizontalInput;
     private bool onGround;
     private Collider2D[] groundHitDetection = new Collider2D[20];
+    private Checkpoint currentCheckpoint;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
         //Debug.Log("This is start!");
-	}
-	
-	// Update is called once per frame
-	void Update () {  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         GetMovementInput();
         GetJump();
         IsOnGround();
@@ -41,32 +52,64 @@ public class PlayerCharacter : MonoBehaviour {
 
     private void FixedUpdate()
     {
+        UpdatePhysiceMaterial();
         Move();
     }
 
-    private void GetMovementInput() {
-        horizontalInput = Input.GetAxis("Horizontal");
-        
+    private void GetMovementInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
     }
 
-    private void Move() {
+    private void Move()
+    {
         CharacterRigid.AddForce(Vector2.right * horizontalInput * accelerationForce);
         Vector2 clampedVelocity = CharacterRigid.velocity;
         clampedVelocity.x = Mathf.Clamp(CharacterRigid.velocity.x, -maxSpeed, maxSpeed);
         CharacterRigid.velocity = clampedVelocity;
     }
-    
+
     private void GetJump()
     {
-        if (Input.GetButtonDown("Jump") && onGround) {
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
             CharacterRigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
-    private void IsOnGround() {
-      onGround = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetection) > 0;
+    private void IsOnGround()
+    {
+        onGround = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetection) > 0;
         //Debug.Log("IsOnGround?: " + onGround);
     }
-    
+
+    private void UpdatePhysiceMaterial()
+    {
+        if (Mathf.Abs(horizontalInput) > 0)
+        {
+            playerGroundCollider.sharedMaterial = playerMoving;
+        }
+        else
+        {
+            playerGroundCollider.sharedMaterial = playerStopping;
+        }
     }
 
+    public void Respawn() {
+        if (currentCheckpoint == null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else {
+            transform.position = currentCheckpoint.transform.position;
+            CharacterRigid.velocity = Vector2.zero;
+        }       
+    }
+
+    public void SetCurrentCheckpoint(Checkpoint newCurrentCheckpoint)
+    {
+        currentCheckpoint = newCurrentCheckpoint;
+    }
+
+}
