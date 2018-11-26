84ef640d380b10ec0ab5cda8a417c70c0f1a89bc
Rigamorphius <43024627+Rigamorphius@users.noreply.github.com>
2018-11-26 15:41:48 -0600

    Added destruction of enemies
    Enemies Die when shot

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+    public static int enemyCount;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+            Destroy(gameObject);
+            enemyCount++;
+            Debug.Log("Enemies defeated is = " + enemyCount);
+        }
+    }
+}
