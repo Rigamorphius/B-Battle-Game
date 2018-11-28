859a9d5db1da5cd610b2ec14465b9efa8d4ed8c9
Rigamorphius <43024627+Rigamorphius@users.noreply.github.com>
2018-11-28 12:29:23 -0600

    Re-adding all my Scripts
    Added my scripts back

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Checkpoint : MonoBehaviour
+{
+
+    private bool isActivated = false;
+    private SpriteRenderer spriteRenderer;
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private float inactivatedRotationSpeed = 100, activatedRotationSpeed = 300;
+
+    [SerializeField]
+    private float inactivatedScale = 1, activatedScale = 2f;
+
+    [SerializeField]
+    private Color inactivatedColor, activatedColor;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateColor();
+    }
+
+    private void Update()
+    {
+        UpdateRotation();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !isActivated)
+        {
+            //Debug.Log("Player entered the checkpoint");
+            PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
+            player.SetCurrentCheckpoint(this);
+            audioSource.Play();
+        }
+    }
+
+    public void SetIsActivated(bool value)
+    {
+        isActivated = value;
+        UpdateScale();
+        UpdateColor();
+    }
+
+    private void UpdateRotation()
+    {
+        float rotationSpeed = inactivatedRotationSpeed;
+
+        if (isActivated)
+        {
+            rotationSpeed = activatedRotationSpeed;
+        }
+
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
+    private void UpdateScale()
+    {
+        float scale = inactivatedScale;
+
+        if (isActivated)
+        {
+            scale = activatedScale;
+        }
+        transform.localScale = Vector3.one * scale;
+    }
+
+    private void UpdateColor()
+    {
+        Color color = inactivatedColor;
+
+        if (isActivated)
+        {
+            color = activatedColor;
+        }
+        spriteRenderer.color = color;
+    }
+
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+    public static int enemyCount;
+    private AudioSource audioSource;
+    Animator anim;
+    private bool isDead;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet") && !isDead)
+        {
+           
+            isDead = true;
+            audioSource.Play();           
+            boxCollider2D.enabled = false;           
+            enemyCount++;
+            Debug.Log("Enemies defeated is = " + enemyCount);
+            anim.SetBool("isDead", isDead);
+            Destroy(collision.gameObject);                  
+        }
+    }
+}
