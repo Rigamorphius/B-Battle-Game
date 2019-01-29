90d1f1a5e47a16146ba035cf29f7474da3e03dbf
Rigamorphius <43024627+Rigamorphius@users.noreply.github.com>
2019-01-28 18:01:53 -0600

    Completed and Built Game

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+}
